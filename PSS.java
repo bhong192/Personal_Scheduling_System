f6602f18fcf1eb58744d94eebc1def2f1e20dbb3
Bryant Hong <59077793+bhong192@users.noreply.github.com>
2021-05-04 19:06:04 -0700

    Add files via upload
    Began defining the classes we'll be using.

---
+++ PSS.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class PSS {
+    
+    ArrayList<Task> taskList = new ArrayList<Task>(); 
+
+    public void createTask(){
+
+        System.out.println("Input the name of your task: \n"); 
+        System.out.println("Input the type of the task: \n"); 
+        System.out.println("Input the : \n"); 
+        System.out.println("Input the name of your task: \n"); 
+        System.out.println("Input the name of your task: \n"); 
+        System.out.println("Input the name of your task: \n"); 
+        System.out.println("Input the name of your task: \n"); 
+
+    }
+
+}
