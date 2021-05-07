43d17217f5af84665aef30426931d68d04b374f1
nj1008 <inehajayan@gmail.com>
2021-05-06 20:48:14 -0500

    Update PSS.java
    Find Task method layout

--- PSS.java
+++ PSS.java
@@ -1,4 +1,6 @@
 import java.util.*;
+
+
 public class PSS {
     
     ArrayList<Task> taskList = new ArrayList<Task>(); 
@@ -14,5 +16,25 @@ public class PSS {
         System.out.println("Input the name of your task: \n"); 
 
     }
+ 
+    public void FindTask(){
+
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Search Task by Name: ");
+        
+        String search = scan.nextLine();
+
+        if (search = taskName) {
+
+            System.out.println(search.Task());
+        }
+
+        scan.close();
+
+    }
+
+
+
+
 
 }
